a30a17d5b4edbb8260af7d1402859ca846632b37
Daniel Jimenez Garcia <daniel.garcia@oliverwyman.com>
2016-07-09 17:35:30 +0100

    Create new ASP Core 1.0 project

---
+++ src/VanityUrls/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VanityUrls.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
