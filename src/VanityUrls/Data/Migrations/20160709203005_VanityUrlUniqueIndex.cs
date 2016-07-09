c1f4c5cdec7443ced4e487ae439901ee777d2796
Daniel Jimenez Garcia <daniel.garcia@oliverwyman.com>
2016-07-09 21:35:15 +0100

    Add unique index on VanityUrls

---
+++ src/VanityUrls/Data/Migrations/20160709203005_VanityUrlUniqueIndex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VanityUrls.Data.Migrations
+{
+    public partial class VanityUrlUniqueIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex("IX_AspNetUsers_VanityUrl", "AspNetUsers", "VanityUrl", unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex("IX_AspNetUsers_VanityUrl");
+        }
+    }
+}
